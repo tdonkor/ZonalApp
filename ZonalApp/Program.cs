d5b5668d2502de177ba5a9021404a1eacb0a9b8f
tdonkor <tdonkor@gmail.com>
2019-04-01 13:11:51 +0100

    save

--- ZonalApp/Program.cs
+++ ZonalApp/Program.cs
@@ -2,7 +2,7 @@
 using RestSharp;
 using System;
 using System.Configuration;
-using System.Net;
+using ZonalApp.classes;
 
 namespace ZonalApp
 {
@@ -11,8 +11,6 @@ namespace ZonalApp
         static void Main(string[] args)
         {
       
-
-
             var client = new RestClient(ConfigurationManager.AppSettings["url"]);
             var request = new RestRequest(Method.POST);
 
@@ -22,24 +20,52 @@ namespace ZonalApp
              request.AddHeader("Accept", "application/json");
              request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
-            //get the venue
+            //get the venue information
              string venueBody = @"{""request"": {""method"":""venues"", ""platform"":""web""}}";
 
-            request.AddParameter("request", venueBody);
+            Console.WriteLine(venueBody);
 
+            //execute the venue body
+             request.AddParameter("request", venueBody);
+
+            //generate the response
             IRestResponse response = client.Execute(request);
 
             //get the venue data first as a string then an object
-            string jsonVenueStr = JsonConvert.SerializeObject(response.Content);
-            dynamic venueData =  JsonConvert.DeserializeObject(response.Content);
+            // string venuesStr = JsonConvert.SerializeObject(response.Content);
+            string venuesStr = JsonConvert.SerializeObject(response.Content);
 
-            Console.WriteLine(jsonVenueStr);
-            Console.WriteLine(venueData);
+            //prepare the class for conversion
+            dynamic venuesData =  JsonConvert.DeserializeObject<dynamic>(response.Content);
 
-            string siteID = venueData["venues"].;
+            //  Console.WriteLine(venuesStr);
+            // Console.WriteLine(venuesData);
 
-            //  string testGetMenuPages = "{""request"": {""method"":""venues"", ""platform"":""web""}}"; "
+            //
+            //Console.WriteLine(venuesData["platform"].ToString());
+            //Console.WriteLine(venuesData["rearMenu"].ToString());
+            //Console.WriteLine(venuesData["services"].ToString());
+            //Console.WriteLine(venuesData["venues"].ToString());
 
+            //get the venue Id details
+            string siteId = venuesData["venues"][0].id.ToString();
+            string salesAreaId = venuesData["venues"][0]["salesArea"][0].id.ToString();
+            string servicesId = venuesData["services"][0].id.ToString();
+
+            Console.WriteLine(" Site Id     = " + siteId);
+            Console.WriteLine(" Area Id     = " + salesAreaId);
+            Console.WriteLine(" Services Id = " + servicesId);
+
+
+
+            //Console.WriteLine(venuesData["count"].ToString());
+            //Console.WriteLine(venuesData["supportsLoyalty"].ToString());
+            //Console.WriteLine(venuesData["response"].ToString());
+
+            //get the menu information
+
+            GetMenus menus = new GetMenus(siteId, salesAreaId, servicesId);
+            menus.GetMenusInformation();
             Console.ReadKey();
         }
     }
---
+++ ZonalApp/classes/GetMenus.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Configuration;
+
+namespace ZonalApp.classes
+{
+    public class GetMenus
+    {
+        string siteId;
+        string salesAreaId;
+        string servicesId;
+
+        public GetMenus(string siteId, string salesAreaId, string servicesId)
+        {
+            this.siteId = siteId;
+            this.salesAreaId = salesAreaId;
+            this.servicesId = servicesId;
+        }
+
+        public void GetMenusInformation()
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["url"]);
+            var request = new RestRequest(Method.POST);
+
+            //header items
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-auth-brandtoken", "Ym91cm5lLWFjcmVsZWMtYmstc3RnOnBhc3N3b3Jk=");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            //get the venue information
+            string getMenusBodyStr = "{\"request\": {\"method\":\"getMenus\", \"siteId\": " +
+                Convert.ToInt32(siteId) + ", " +
+                 "\"salesAreaId\" : " +
+                Convert.ToInt32(salesAreaId) +
+                ", \"platform\" : \"web\"" +
+                ", \"servicesId\" : " +
+                Convert.ToInt32(servicesId) +
+                "}}";
+
+            Console.WriteLine(getMenusBodyStr);
+
+            //execute the Menus body
+            request.AddParameter("request", getMenusBodyStr);
+
+            //generate the response
+            IRestResponse response = client.Execute(request);
+
+            dynamic getMenusData = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+            Console.WriteLine(getMenusData);
+        }
+    }
+    
+}
